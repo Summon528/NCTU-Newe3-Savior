735fa4e5efb87d57ee9bccb3f6cef26031ed1d2a
CodyTseng <cody880528@hotmail.com>
2019-03-04 23:37:26 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: {
+        'index': './src/index.ts',
+        'hide_loading': './src/hide_loading'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
