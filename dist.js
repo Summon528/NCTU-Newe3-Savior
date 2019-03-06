bb8f31eadcaa3ee09f0f1ee268160ee9f0a61d3c
CodyTseng <cody880528@hotmail.com>
2019-03-06 14:03:32 +0800

    add dist.js

---
+++ dist.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const archiver = require('archiver');
+
+const output = fs.createWriteStream(__dirname + '/dist.zip');
+const archive = archiver('zip');
+
+output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+output.on('end', function () {
+    console.log('Data has been drained');
+});
+
+archive.on('warning', function (err) {
+    if (err.code === 'ENOENT') {
+        console.warn(err);
+    } else {
+        throw err;
+    }
+});
+
+archive.on('error', function (err) {
+    throw err;
+});
+
+archive.pipe(output);
+
+archive.file('manifest.json', { name: 'manifest.json' });
+archive.directory('dist/', 'dist');
+archive.finalize();
